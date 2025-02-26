59177f652cd05b108570783707340b535d42aad5
Wilian-N-Silva <wilian.silva209@gmail.com>
2025-02-26 15:14:38 -0300

    refactor: Code refactor

--- animation.js
+++ animation.js
@@ -4,48 +4,41 @@ let cards = gsap.utils.toArray(".card");
 
 cards.forEach((card, index) => {
     let offset = (index % 2 === 0) ? 135 : -135;
+    const baseY = window.innerHeight * 0.25
+    const baseX = window.innerWidth * 0.25
 
     gsap.set(card, {
         x: offset,
-        // toggleActions: 'play none none reverse',
     });
+
 
     gsap.to(card, {
         x: 0,
+        // zIndex: index-=10,
         scrollTrigger: {
             trigger: card,
-            start: "center 800",
-            end: "center center",
+            start: 'top bottom-=300',
+            end: "top bottom-=300",
             markers: true,
-            // toggleActions: 'play none none reverse',
+
+
         }
     });
 
-    const baseX = window.innerWidth * 0.25
-    const baseY = window.innerHeight * 0.25
 
     gsap.to(card, {
-        x: (index % 2 === 0) ? -(baseX + index * 25) : (baseX + index * 25), scrollTrigger: {
+        x: (index % 2 === 0) ? -(baseX + index * 25) : (baseX + index * 25),
+        scrollTrigger: {
             trigger: card,
-            markers: true,  
+            markers: true,
             markers: {
                 startColor: "blue",
                 endColor: "brown"
-              },
+            },
             start: `top ${baseY}`,
             end: "center center",
             duration: .5,
             toggleActions: 'play none none reverse',
         },
     });
-
-    // gsap.to(card, {
-    //     opacity: 0, scrollTrigger: {
-    //         trigger: card,
-    //         start: "top 10%",
-    //         end: "top top",
-    //         toggleActions: 'play none none reverse',
-
-    //     },
-    // });
 });
