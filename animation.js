9e350ca295368c23c10c098654d8cc78274d1b20
Wilian-N-Silva <wilian.silva209@gmail.com>
2025-02-26 13:32:45 -0300

    feat: New animation style with sticky container

--- animation.js
+++ animation.js
@@ -5,33 +5,47 @@ let cards = gsap.utils.toArray(".card");
 cards.forEach((card, index) => {
     let offset = (index % 2 === 0) ? 135 : -135;
 
-    gsap.set(card, { x: offset });
+    gsap.set(card, {
+        x: offset,
+        // toggleActions: 'play none none reverse',
+    });
 
     gsap.to(card, {
         x: 0,
         scrollTrigger: {
             trigger: card,
-            start: "bottom bottom",
+            start: "center 800",
             end: "center center",
+            markers: true,
+            // toggleActions: 'play none none reverse',
         }
     });
 
+    const baseX = window.innerWidth * 0.25
+    const baseY = window.innerHeight * 0.25
+
     gsap.to(card, {
-        x: (index % 2 === 0) ? `-50vw` : `50vw`, scrollTrigger: {
+        x: (index % 2 === 0) ? -(baseX + index * 25) : (baseX + index * 25), scrollTrigger: {
             trigger: card,
-            start: "center center",
-            end: "top top",
+            markers: true,  
+            markers: {
+                startColor: "blue",
+                endColor: "brown"
+              },
+            start: `top ${baseY}`,
+            end: "center center",
+            duration: .5,
             toggleActions: 'play none none reverse',
         },
     });
 
-    gsap.to(card, {
-        opacity: 0, scrollTrigger: {
-            trigger: card,
-            start: "top 10%",
-            end: "top top",
-            toggleActions: 'play none none reverse',
+    // gsap.to(card, {
+    //     opacity: 0, scrollTrigger: {
+    //         trigger: card,
+    //         start: "top 10%",
+    //         end: "top top",
+    //         toggleActions: 'play none none reverse',
 
-        },
-    });
+    //     },
+    // });
 });
